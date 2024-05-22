75dee27c1621195e319f3ba9545f9643a3739f5c
Ella Afonso <daniella.afonso@outlook.com>
2024-05-23 00:38:05 +0100

    data ingestion added

--- demo.py
+++ demo.py
@@ -1,10 +1,7 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-import sys 
+from us_visa.pipeline.training_pipeline import TrainPipeline
+
+pipeline = TrainPipeline()
+pipeline.run_pipeline()
 
 
-try: 
-    a = 1 / "10"
-except Exception as e:
-    logging.info(e)
-    raise USvisaException(e, sys) from e
+
